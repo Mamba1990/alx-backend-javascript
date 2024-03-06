fc272407ee698f737150fcbebc6e90e7a0d1fba7
Maba1990 <h.daoudim@outlook.com>
2024-03-06 20:01:59 +0000

    task-06

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,20 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((results) => {
-    const response = [];
-
-    results.forEach((result) => {
-      response.push({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason,
-      });
-    });
-
-    return response;
-  });
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }

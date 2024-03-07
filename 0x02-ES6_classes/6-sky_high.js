98cc10b6ab218f568423ae1f23f30fd897602e07
Mamba1990 <h.daoudim@outlook.com>
2024-03-07 13:58:50 +0000

    6-sky_high.js

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-underscore-dangle */
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(fl) {
+    if (typeof fl !== 'number') {
+      throw new Error('Floors must be a number');
+    }
+    this._floors = fl;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}

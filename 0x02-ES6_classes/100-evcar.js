7031ff026d7370b7a1ba71921978049ee553ae25
Mamba1990 <h.daoudim@outlook.com>
2024-03-07 14:48:59 +0000

    100

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -4,14 +4,20 @@ import Car from './10-car';
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this._range = range;
+    this.range = range;
   }
 
   get range() {
     return this._range;
   }
 
-  static get [Symbol.species]() {
-    return Car;
+  set range(value) {
+    this._range = value;
+  }
+
+  cloneCar() {
+    const Species = super.constructor[Symbol.species];
+
+    return new Species();
   }
 }

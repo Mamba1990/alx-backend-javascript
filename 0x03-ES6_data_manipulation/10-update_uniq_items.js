995b30baf7e35ce314a8326a7cfdac254eab31fb
Mamba1990 <h.daoudim@outlook.com>
2024-03-11 15:45:42 +0000

    10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,15 +1,13 @@
-/**
- * Retrieves a map of grocery names and their quantity.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
- * @returns {Map<string, Number>}
- */
-export default function groceriesList() {
-  const values = [
-    ['Apples', 10],
-    ['Tomatoes', 10],
-    ['Pasta', 1],
-    ['Rice', 1],
-    ['Banana', 5],
-  ];
-  return new Map(values);
+export default function updateUniqueItems(groceriesList) {
+  try {
+    for (const [key, value] of groceriesList) {
+      if (value === 1) {
+        groceriesList.set(key, 100);
+      }
+    }
+  } catch (error) {
+    throw Error('Cannot process');
+  }
+
+  return groceriesList;
 }
